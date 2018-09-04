0f857fdde04c0bb57872fb9a8e33101524bd4ccb
swolfenb <swolfenbarger@bodhiserver.info>
2018-09-03 23:11:31 -0400

    Updated Price column currency formatting. Added jquery.dataTables.min.css and related images.

--- BookStore/wwwroot/js/site.js
+++ BookStore/wwwroot/js/site.js
@@ -21,23 +21,43 @@ $(document).ready(function () {
         "columnDefs": [
             {
                 "targets": 0,
-                "data": "ISBN",
                 "render": function (url, type, full) {
                     return '<a href="https://www.edelweiss.plus/#sku=' + full.rowKey + '" target="_blank">' + full.rowKey + '</a>';
                 }
             },
             {
                 "targets": 1,
-                "data": "jacket",
+                "className": 'dt-body-center'
+            },
+            {
+                "targets": 1,
                 "render": function (url, type, full) {
-                    return '<img src="' + full.jacketUrl + '?width=50"/>';
+                    return '<img src="' + full.jacketUrl + '?width=50" title="'+full.title+'"/>';
                 }
             },
             {
                 "targets": 5,
-                "data": "price",
+                "className": 'dt-body-right',
                 "render": function (url, type, full) {
-                    return '$' + full.price;
+                    // If there is no price, return a blank
+                    if (!full.price) {
+                        return "";
+                    }
+
+                    // Ensure full.price is a string, and split on decimal point
+                    full.price += '';
+                    x = full.price.split('.');
+                    x1 = x[0];
+                    x2 = x.length > 1 ? '.' + x[1] : '.00';
+
+                    // Add thousand separater as needed
+                    var rgx = /(\d+)(\d{3})/;
+                    while (rgx.test(x1)) {
+                        x1 = x1.replace(rgx, '$1' + ',' + '$2');
+                    }
+
+                    // Return formatted currency
+                    return "$" + x1 + x2;
                 }
             }
         ]
